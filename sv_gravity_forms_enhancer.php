64ca311be5514aa8806fd3e9ad17c6a0bdf61cad
Matthias Reuter <info@straightvisions.com>
2018-10-09 20:01:54 +0200

    BUG - FIX - Compatibility Fix for gravityformsuserregistration

--- sv_gravity_forms_enhancer.php
+++ sv_gravity_forms_enhancer.php
@@ -3,7 +3,7 @@
 	Plugin Name: SV Gravity Forms Enhancer
 	Plugin URI: https://straightvisions.com/
 	Description: Improves Gravity Forms in various ways.
-	Version: 1.0.5
+	Version: 1.0.6
 	Author: Matthias Reuter
 	Author URI: https://straightvisions.com
 	Text Domain: sv_gravity_forms_enhancer
@@ -16,7 +16,7 @@
 	require_once('lib/core/core.php');
 
 	class init extends \sv_core\core{
-		const version							= 1005;
+		const version							= 1006;
 		const version_core_match				= 1000;
 
 		public function __construct(){
