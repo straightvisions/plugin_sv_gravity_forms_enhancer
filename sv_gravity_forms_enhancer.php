3760b67e914817f6fbf9667abbc468b3015afd2d
dennisheiden <heiden@straightvisions.com>
2019-02-19 11:03:33 +0100

    Revert "Merge remote-tracking branch 'origin/default' into dev"
    caused by: https://brainbirds.atlassian.net/browse/BB-250
    
    This reverts commit 5acbe9ccf0e3a054266dcc59a27a2b2470cb392c, reversing
    changes made to 1f8062f2ebb14186c78ed7a36e66d4dc944a84ce.

--- sv_gravity_forms_enhancer.php
+++ sv_gravity_forms_enhancer.php
@@ -3,8 +3,8 @@
 	Plugin Name: SV Gravity Forms Enhancer
 	Plugin URI: https://straightvisions.com/
 	Description: Improves Gravity Forms in various ways.
-	Version: 1.1.0
-	Author: straightvisions GmbH
+	Version: 1.0.8
+	Author: Matthias Reuter
 	Author URI: https://straightvisions.com
 	Text Domain: sv_gravity_forms_enhancer
 	License: GPL3
@@ -16,23 +16,13 @@
 	require_once('lib/core/core.php');
 
 	class init extends \sv_core\core{
-		const version							= 1100;
-		const version_core_match				= 1010;
-		
-		/**
-		 * @desc			Basic setup & information
-		 * @return	void
-		 * @author			Matthias Bathke
-		 * @since			1.0
-		 */
+		const version							= 1008;
+		const version_core_match				= 1005;
+
 		public function __construct(){
 			$this->setup(__NAMESPACE__,__FILE__);
 			$this->set_section_title('SV Gravity Forms Enhancer');
 			$this->set_section_desc('Improves Gravity Forms in various ways');
-			$this->set_section_privacy('<p>
-				'.$this->get_section_title().' does not collect or share any data from clients or visitors, but integrates into Gravity Forms plugin - that is a form plugin which natural purpose is to aggregate data.<br />
-				'.$this->get_section_title().' does not extend the original behavior of Gravity Forms plugin, so there is no additional userdata retrieved through '.$this->get_section_title().'.
-			</p>');
 		}
 	}
 
