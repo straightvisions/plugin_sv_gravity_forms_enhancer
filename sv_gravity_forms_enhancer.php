078ae561076ad7935a9899a7e9911a43feedbe23
Matthias Reuter <matthias-reuter@straightvisions.com>
2019-05-03 14:44:30 +0200

    UPDATE CORE

--- sv_gravity_forms_enhancer.php
+++ sv_gravity_forms_enhancer.php
@@ -3,7 +3,7 @@
 	Plugin Name: SV Gravity Forms Enhancer
 	Plugin URI: https://straightvisions.com/
 	Description: Improves Gravity Forms in various ways.
-	Version: 1.3.05
+	Version: 1.3.06
 	Author: straightvisions GmbH
 	Author URI: https://straightvisions.com
 	Text Domain: sv_gravity_forms_enhancer
@@ -16,8 +16,8 @@
 	require_once('lib/core/core.php');
 
 	class init extends \sv_core\core{
-		const version							= 1305;
-		const version_core_match				= 3101;
+		const version							= 1306;
+		const version_core_match				= 3103;
 		
 		/**
 		 * @desc			Basic setup & information
