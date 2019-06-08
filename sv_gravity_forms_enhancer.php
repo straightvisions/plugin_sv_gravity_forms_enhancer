6748d005090b2faee9da34b44e75f29e74018d6e
Matthias Reuter <info@straightvisions.com>
2019-06-08 15:41:28 +0200

    UPDATE - CORE

--- sv_gravity_forms_enhancer.php
+++ sv_gravity_forms_enhancer.php
@@ -1,5 +1,4 @@
 <?php
-	
 	/*
 	Plugin Name: SV Gravity Forms Enhancer
 	Description: Improves Gravity Forms in various ways.
@@ -16,27 +15,32 @@
 
 	namespace sv_gravity_forms_enhancer;
 
-	require_once('lib/core/core.php');
+	$min_php = '7.0.0';
+	$name = 'SV ProvenExpert';
 
-	class init extends \sv_core\core{
-		const version							= 1310;
-		const version_core_match				= 3110;
-		
-		/**
-		 * @desc			Basic setup & information
-		 * @return	void
-		 * @author			Matthias Bathke
-		 * @since			1.0
-		 */
-		public function __construct(){
-			$this->setup(__NAMESPACE__,__FILE__);
-			$this->set_section_title('SV Gravity Forms Enhancer');
-			$this->set_section_desc('Improves Gravity Forms in various ways');
-			$this->set_section_privacy('<p>
-				'.$this->get_section_title().' does not collect or share any data from clients or visitors, but integrates into Gravity Forms plugin - that is a form plugin which natural purpose is to aggregate data.<br />
-				'.$this->get_section_title().' does not extend the original behavior of Gravity Forms plugin, so there is no additional userdata retrieved through '.$this->get_section_title().'.
-			</p>');
+	if(version_compare( phpversion(), $min_php, '>=' )) {
+		class init extends \sv_core\core{
+			const version							= 1310;
+			const version_core_match				= 3126;
+
+			/**
+			 * @desc			Basic setup & information
+			 * @return	void
+			 * @author			Matthias Bathke
+			 * @since			1.0
+			 */
+			public function __construct(){
+				if(!$this->setup( __NAMESPACE__, __FILE__ )){
+					return false;
+				}
+				$this->set_section_title('SV Gravity Forms Enhancer');
+				$this->set_section_desc('Improves Gravity Forms in various ways');
+				$this->set_section_privacy('<p>
+					'.$this->get_section_title().' does not collect or share any data from clients or visitors, but integrates into Gravity Forms plugin - that is a form plugin which natural purpose is to aggregate data.<br />
+					'.$this->get_section_title().' does not extend the original behavior of Gravity Forms plugin, so there is no additional userdata retrieved through '.$this->get_section_title().'.
+				</p>');
+			}
 		}
-	}
 
-	$GLOBALS[__NAMESPACE__]			= new init();
+		$GLOBALS[__NAMESPACE__]			= new init();
+	}
