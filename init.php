f0e94c138bfe5ba9d10fec530497171db8e35c99
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-11-02 17:11:46 +0100

    UPDATE CORE

--- init.php
+++ init.php
@@ -6,8 +6,8 @@
 	}
 	
 	class init extends \sv_core\core_plugin {
-		const version = 1406;
-		const version_core_match = 4031;
+		const version = 1510;
+		const version_core_match = 5100;
 		
 		public function load(){
 			if(!$this->setup( __NAMESPACE__, __FILE__ )){
--- sv_gravity_forms_enhancer.php
+++ sv_gravity_forms_enhancer.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Version: 1.4.06
+Version: 1.5.1ß
 Plugin Name: SV Gravity Forms Enhancer
 Text Domain: sv_gravity_forms_enhancer
 Description: Improves Gravity Forms in various ways.
